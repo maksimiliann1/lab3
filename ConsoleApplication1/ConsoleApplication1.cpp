2232a98fb0cfb3d645d98fa8e41ad5e92bec391e
79160 <79160@LAPTOP-M8JOIHHO>
2022-12-05 17:12:14 +0300

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,39 @@
+﻿
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <regex>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    ifstream htmlF;
+    htmlF.open("C:/proga/кодинг/lab3/ConsoleApplication1/index.html");
+    string str;
+    if (!htmlF.is_open()) {
+        cout << "unlucky";
+    }
+    else {
+        cout << "lucky"<<endl;
+        while(!htmlF.eof()){
+            str = "";
+            htmlF >> str;
+            smatch result;
+            regex regular("([\\w-]+)""(@)""([\\w]+)""(\.)""([a-z]{2,5})");
+            
+                
+            regex_search(str, result, regular);
+
+            if (!result.empty()) {
+                cout << result[0] << endl;
+            }
+            
+        }
+        
+
+    }
+    htmlF.close();
+}
+
